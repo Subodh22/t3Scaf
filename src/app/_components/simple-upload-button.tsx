d66759695e82e363073cdd2b5124f6e9bc1cefe0
Subodh22 <SubodhMaharjan33@gmail.com>
2024-06-18 12:01:05 +1000

    added toast

--- src/app/_components/simple-upload-button.tsx
+++ src/app/_components/simple-upload-button.tsx
@@ -2,6 +2,7 @@
 
 import { useRouter } from "next/navigation";
 import { useUploadThing } from "~/utils/uploadthing";
+import { toast } from "sonner";
 
 // inferred input off useUploadThing
 type Input = Parameters<typeof useUploadThing>;
@@ -43,8 +44,18 @@ function UploadSvg(){
 export function SimpleUploadButton (){
     const router = useRouter()
     const {inputProps} = useUploadThingInputProps("imageUploader",{
+        onUploadBegin(){
+            toast("Uploading..."),
+          {
+            
+            id:"upload-begin",
+          }
+        },
         onClientUploadComplete(){
+          toast.dismiss("upload-begin")
+          toast("Upload Complete")
             router.refresh()
+           
         }
     })
 return (
