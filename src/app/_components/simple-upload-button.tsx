c88864e50934de79475cab22531ffc544291d584
Subodh22 <SubodhMaharjan33@gmail.com>
2024-06-23 11:19:23 +1000

    ratelimit added

--- src/app/_components/simple-upload-button.tsx
+++ src/app/_components/simple-upload-button.tsx
@@ -66,6 +66,12 @@ export function SimpleUploadButton (){
             id:"upload-begin",
           }
         },
+        onUploadError(error){
+          posthog.capture("upload error",{error});
+          toast.dismiss("upload-begin")
+          toast.error("Upload failed")
+        }
+        ,
         onClientUploadComplete(){
           toast.dismiss("upload-begin")
           toast("Upload Complete")
